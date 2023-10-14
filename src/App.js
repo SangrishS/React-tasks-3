28861c1383132ecb09483c0bfaebb3df4844b3ea
SangrishS <sangrish.saigiridhar2021@vitstudent.ac.in>
2023-10-14 18:31:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const colors = ["#FF5733", "#C70039", "#900C3F", "#581845", "#FFC300", "#DAF7A6", "#FF5733", "#C70039", "#900C3F"];
+
+const ColorPicker = () => {
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const changeColor = (color) => {
+    setSelectedColor(color);
+  };
+
+  const containerStyle = {
+    backgroundColor: selectedColor,
+  };
+
+  const colorSquares = colors.map((color, index) => (
+    <div
+      key={index}
+      className="color-square"
+      style={{ backgroundColor: color }}
+      onClick={() => changeColor(color)}
+    ></div>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="color-picker">
+        <h2>Color Picker:</h2>
+        <div className="color-container" style={containerStyle}>
+          {colorSquares}
+        </div>
+        <div backgroundColorcolor='green'>
+          <div className='outer-cover'>
+            <h3>Pick a Color</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default ColorPicker;
